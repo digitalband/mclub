019c36ba92028cac247b0e38bd4097e1945e0092
ivan-karaberov <karaberov.ivan@yandex.ru>
2024-11-26 00:00:53 +0300

    style: added padding

--- src/api_v1/auth/utils.py
+++ src/api_v1/auth/utils.py
@@ -6,28 +6,21 @@ import bcrypt
 
 
 class JWTHandler:
-    def __init__(
-        self,
-        private_key: str,
-        public_key: str,
-        algorithm: str
-    ) -> None:
+    def __init__(self, private_key: str, public_key: str, algorithm: str) -> None:
         self.private_key = private_key
         self.public_key = public_key
         self.algorithm = algorithm
 
     def encode(
-        self, 
-        payload: dict[str, Any],
-        expire_token: Union[int, timedelta]
+        self, payload: dict[str, Any], expire_token: Union[int, timedelta]
     ) -> str:
         """
         Encodes data into a JWT token
 
         Args:
             payload (dict[str, Any]): A dictionary with the data to be encoded
-                                      into the token.    
-            expire_token (Union[int, timedelta]): The time for the token to 
+                                      into the token.
+            expire_token (Union[int, timedelta]): The time for the token to
                 expire. Can be specified in minutes (int) or as timedelta.
 
         Returns:
@@ -37,20 +30,15 @@ class JWTHandler:
         now = datetime.now(timezone.utc)
 
         if isinstance(expire_token, int):
-            expire = now+timedelta(minutes=expire_token)
+            expire = now + timedelta(minutes=expire_token)
         elif isinstance(expire_token, timedelta):
             expire = now + expire_token
         else:
             raise ValueError("expire_token must be an int or timedelta")
-        
-        to_encode.update(
-            exp=expire,
-            iat=now
-        )
+
+        to_encode.update(exp=expire, iat=now)
         return jwt.encode(
-            payload=to_encode,
-            key=self.private_key,
-            algorithm=self.algorithm
+            payload=to_encode, key=self.private_key, algorithm=self.algorithm
         )
 
     def decode(self, token: str) -> dict[str, Any]:
@@ -63,11 +51,7 @@ class JWTHandler:
         Returns:
             dict[str, Any]: The decoded payload as a dictionary.
         """
-        return jwt.decode(
-            jwt=token,
-            key=self.public_key,
-            algorithms=self.algorithm
-        )
+        return jwt.decode(jwt=token, key=self.public_key, algorithms=self.algorithm)
 
 
 class PasswordHandler:
@@ -75,23 +59,23 @@ class PasswordHandler:
     def hash(password: str, rounds: int = 12) -> bytes:
         """
         Hash a plain text password.
-      
+
         Args:
             password (str): The plain text password to be hashed.
             rounds (int, optional): The number of hashing rounds. Default is 12.
-                Increasing the number of rounds makes the hashing process 
-                slower, which enhances security by making brute-force attacks 
-                more difficult. More rounds mean that it takes longer to 
-                compute the hash, thus increasing the time required for an 
-                attacker to guess the password through repeated attempts. 
-                It is recommended to choose a value between 10 and 14, 
-                depending on the security requirements and performance 
+                Increasing the number of rounds makes the hashing process
+                slower, which enhances security by making brute-force attacks
+                more difficult. More rounds mean that it takes longer to
+                compute the hash, thus increasing the time required for an
+                attacker to guess the password through repeated attempts.
+                It is recommended to choose a value between 10 and 14,
+                depending on the security requirements and performance
                 considerations.
 
         Returns:
             bytes: The hashed password as a byte string.
         """
-        return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt(rounds))
+        return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt(rounds))
 
     @staticmethod
     def validate(password: str, hashed_password: bytes) -> bool:
@@ -103,10 +87,9 @@ class PasswordHandler:
             hashed_password (bytes): The hashed password to compare against.
 
         Returns:
-            bool: True if the password matches the hashed password, 
+            bool: True if the password matches the hashed password,
                   False otherwise.
         """
         return bcrypt.checkpw(
-            password=password.encode(),
-            hashed_password=hashed_password
+            password=password.encode(), hashed_password=hashed_password
         )
