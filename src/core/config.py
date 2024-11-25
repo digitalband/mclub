94acc6bd9611dff619464b3160cd435f1158883b
ivan-karaberov <karaberov.ivan@yandex.ru>
2024-11-25 20:20:17 +0300

    feat: add config file

---
+++ src/core/config.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+BASE_DIR = Path(__file__).parent.parent
+
+
+class DbSettings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    @property
+    def DB_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="allow"
+    )
+
+
+class AuthJWTSettings(BaseSettings):
+    private_key_path: Path = BASE_DIR / "core" / "certs" / "jwt-private.pem"
+    public_key_path: Path = BASE_DIR / "core" / "certs" / "jwt-public.pem"
+    algorithm: str = "RS256"
+    access_token_expire_minutes: int = 30
+
+
+class Settings(BaseSettings):
+    MODE: str
+    db: DbSettings = Field(default_factory=DbSettings)
+    auth_jwt: AuthJWTSettings = Field(default_factory=AuthJWTSettings)
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+    )
+
+
+settings = Settings()
