751985d4733fe4742cf9c1f14b58950b359ea6a8
ivan-karaberov <karaberov.ivan@yandex.ru>
2024-11-26 19:34:58 +0300

    feat: add migrations

--- src/core/config.py
+++ src/core/config.py
@@ -52,7 +52,7 @@ class Settings(BaseSettings):
 
     model_config = SettingsConfigDict(
         env_file=ENV_FILE_PATH,
-        env_file_encoding="utf-8",
+        env_file_encoding="utf-8"
     )
 
 
