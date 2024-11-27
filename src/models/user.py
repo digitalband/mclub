4f0974e9db4f5f311b9450d3a066f42aa61f1dcc
ivan-karaberov <karaberov.ivan@yandex.ru>
2024-11-27 18:52:45 +0300

    feat: add users table

---
+++ src/models/user.py
@@ -0,0 +1,18 @@
+from sqlalchemy import func, String, LargeBinary, Boolean, DateTime, TIMESTAMP
+from sqlalchemy.orm import Mapped, mapped_column
+
+from models.base import Base
+
+class User(Base):
+    firstName: Mapped[str] = mapped_column(String(50), nullable=False)
+    lastName: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String, nullable=False)
+    role: Mapped[str] = mapped_column(String(8), nullable=False)
+    phone: Mapped[str] = mapped_column(String(30), nullable=True)
+    hashed_password: Mapped[bytes] = mapped_column(LargeBinary, nullable=True)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, server_default='True')
+    created_at: Mapped[DateTime] = mapped_column(
+        TIMESTAMP(timezone=True),
+        default=func.now(),
+        server_default=func.now()
+    )
